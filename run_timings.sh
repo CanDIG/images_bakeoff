11579b5ae1dff3dd39ea0b8ae3b89f5909a22353
Jonathan Dursi <jonathan@dursi.ca>
2017-01-20 16:57:10 -0500

    Fix samtools sort line, quote vars

--- run_timings.sh
+++ run_timings.sh
@@ -9,7 +9,7 @@ do
     echo "$count: $bam"
     outdir="${HOME}/out/${count}"
     mkdir -p "${outdir}"
-    cd "${outdir}"
+    cd "${outdir}" || exit
 
     cp "${HOME}/data/hg38.fa.gz" .
     cp "${HOME}/data/hg38.fa.gz.fai" .
@@ -18,7 +18,7 @@ do
     perf-report -o "bwa-${count}" /usr/bin/bwa index hg38.fa.gz
 
     echo "samtools view"
-    perf-report -o "samtools-view-${count}" /usr/bin/samtools view -hf 0x2 ${bam}  > input.bam
+    perf-report -o "samtools-view-${count}" /usr/bin/samtools view -hf 0x2 "${bam}"  > input.bam
 
     echo "picard samtofastq"
     perf-report -o "samtofastq-view-${count}" /usr/bin/java -Xmx4g -jar ${PATH_TO_PICARD}/picard.jar SamToFastq VALIDATION_STRINGENCY=LENIENT I=input.bam F=file_1.fastq F2="file_2.fastq"
@@ -27,8 +27,8 @@ do
     perf-report -o "bwamem-${count}" /usr/bin/bwa mem hg38.fa.gz file_1.fastq file_2.fastq | samtools view -bS - > output.bam
     
     echo "samtools sort"
-    perf-report -o "samtools-sort-${count}" /usr/bin/samtools sort -m 1M output.bam > sorted.bam
+    perf-report -o "samtools-sort-${count}" /usr/bin/samtools sort -m 1M output.bam sorted
 
-    rm *.fastq output.bam sorted.bam input.bam hg38.fa.*
+    rm -- *.fastq output.bam sorted.bam input.bam hg38.fa.*
     count=$((count+1))
 done
