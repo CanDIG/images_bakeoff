3ed6a8baba0197ab7dce2a486271cb105e00ba94
Jonathan Dursi <jonathan@dursi.ca>
2017-02-01 14:48:22 -0600

    Fix singularity launches: add SamToFastq to picard runline, remove references to docker mapped dirs, fix typo in rm line

--- run_singularity_timings.sh
+++ run_singularity_timings.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+CARD=${HOME}/singularities/ubuntu16-picard.img/data/$( basename ${bam} )  #!/usr/bin/env bash
 
 readonly DATA="${HOME}/data"
 readonly OUT="${HOME}/singularity_out"
@@ -38,10 +38,10 @@ do
     /usr/bin/time -p -a -o "$logfile" singularity run ${BWA} index hg38.fa.gz 
 
     log "$logfile" "samtools view"
-    /usr/bin/time -p -a -o "$logfile" singularity run ${SAMTOOLS} view -hf 0x2 /data/$( basename ${bam} )  > input.bam
+    /usr/bin/time -p -a -o "$logfile" singularity run ${SAMTOOLS} view -hf 0x2 "$bam" > input.bam
 
     log "$logfile" "picard samtofastq"
-    /usr/bin/time -p -a -o "$logfile" singularity run ${PICARD} VALIDATION_STRINGENCY=LENIENT I=input.bam F=file_1.fastq F2="file_2.fastq"
+    /usr/bin/time -p -a -o "$logfile" singularity run ${PICARD} SamToFastq VALIDATION_STRINGENCY=LENIENT I=input.bam F="file_1.fastq" F2="file_2.fastq"
 
     log "$logfile" "bwa mem"
     /usr/bin/time -p -a -o "$logfile" singularity run ${BWA} mem hg38.fa.gz file_1.fastq file_2.fastq | singularity run ${SAMTOOLS} view -bS - > output.bam
@@ -56,7 +56,7 @@ do
     /usr/bin/time -p -a -o "$logfile" singularity exec ${SAMTOOLS} /usr/local/bin/samtools_random.sh viewlines.txt | wc -l
 
     log "$logfile" "rm"
-    /usr/bin/time -p -a -o "$logfile" singularity exec ${BWA} /bin/rm -ubuntu -f file_1.fastq file_2.fastq output.bam sorted.bam sorted.bam.bai input.bam hg38.fa.gz hg38.fa.gz.amb hg38.fa.gz.ann hg38.fa.gz.bwt hg38.fa.gz.fai hg38.fa.gz.pac hg38.fa.gz.sa viewlines.txt
+    /usr/bin/time -p -a -o "$logfile" singularity exec ${BWA} /bin/rm -f file_1.fastq file_2.fastq output.bam sorted.bam sorted.bam.bai input.bam hg38.fa.gz hg38.fa.gz.amb hg38.fa.gz.ann hg38.fa.gz.bwt hg38.fa.gz.fai hg38.fa.gz.pac hg38.fa.gz.sa viewlines.txt
 
     count=$((count+1))
 done
